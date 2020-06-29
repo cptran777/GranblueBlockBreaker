3a2035f53b4d4d4ce605488a84d3cfcc7588aafa
Charlie Tran <cptran14@gmail.com>
2020-06-29 13:08:36 -0700

    Move paddle with mouse or a and d

--- Assets/Scripts/PaddleHandler.cs
+++ Assets/Scripts/PaddleHandler.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PaddleHandler : MonoBehaviour {
+    [Tooltip("Increased sensitivity to move the paddle based on input")]
+    [SerializeField] float movementMultiplier = 1f;
+
     void Start() {
         print("We're starting?");
     }
 
     void Update() {
-        Debug.Log(Input.mousePosition);
+        MovePaddle();
+    }
+
+    private void MovePaddle() {
+        // Normalizes the possibility of sensitive mice going over
+        float deltaMovement = Mathf.Max(Mathf.Min(1, CrossPlatformInputManager.GetAxis("Horizontal")), -1);
+        // Offset for framerate
+        deltaMovement = deltaMovement * Time.deltaTime * movementMultiplier;
+        float currentPosition = transform.position.x;
+        // 0f for y since the paddle only moves in 2 directions
+        transform.position = new Vector2(currentPosition + deltaMovement, transform.position.y);
     }
 }
